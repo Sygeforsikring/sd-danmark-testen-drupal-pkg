0433bdac5095a6155d1edbe8236b5fbe9b58b2f8
meandersen <mathias.andersen@adapt.dk>
2020-05-26 12:09:14 +0200

    Release 2.0.16

---
+++ precache-manifest.f281f152955cbf7ff65a308133f027dd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1a4ecdf6b46c365e109",
+    "url": "/libraries/danmark-testen/static/css/main.b9f32606.chunk.css"
+  },
+  {
+    "revision": "b1a4ecdf6b46c365e109",
+    "url": "/libraries/danmark-testen/static/js/main.b1a4ecdf.chunk.js"
+  },
+  {
+    "revision": "ff4dbe425a1e84a6b685",
+    "url": "/libraries/danmark-testen/static/js/1.ff4dbe42.chunk.js"
+  },
+  {
+    "revision": "79ce4cc3e6f12e9cb44b",
+    "url": "/libraries/danmark-testen/static/js/runtime~main.79ce4cc3.js"
+  },
+  {
+    "revision": "745ab4ede44e4c88aa8dda40e1115ad3",
+    "url": "/libraries/danmark-testen/static/media/icons__dot--grey.745ab4ed.svg"
+  },
+  {
+    "revision": "bff5b07ccbc3c7422b2b060c7a2bb058",
+    "url": "/libraries/danmark-testen/static/media/icons__dot--red.bff5b07c.svg"
+  },
+  {
+    "revision": "a555067a204023a14ae6f65741a16fd7",
+    "url": "/libraries/danmark-testen/static/media/icons__heart--grey.a555067a.svg"
+  },
+  {
+    "revision": "8837f62ead22edef140eb67b6e52d994",
+    "url": "/libraries/danmark-testen/static/media/icons__heart--red.8837f62e.svg"
+  },
+  {
+    "revision": "007f057987db59e1ce358cabf373bff7",
+    "url": "/libraries/danmark-testen/static/media/step-intro-illustration.007f0579.svg"
+  },
+  {
+    "revision": "48180df301311206eedc407d34615fe6",
+    "url": "/libraries/danmark-testen/static/media/montserrat-regular.48180df3.eot"
+  },
+  {
+    "revision": "fb9f212b6362c158fd1d82514cb64281",
+    "url": "/libraries/danmark-testen/static/media/montserrat-regular.fb9f212b.woff2"
+  },
+  {
+    "revision": "439848559b8c1b40ee7a0b68ac756808",
+    "url": "/libraries/danmark-testen/static/media/montserrat-regular.43984855.woff"
+  },
+  {
+    "revision": "4aec0152c75757d9c349f0a1df203cbf",
+    "url": "/libraries/danmark-testen/static/media/montserrat-bold.4aec0152.eot"
+  },
+  {
+    "revision": "5704e95d4ffd15d3c9466515fb5612c8",
+    "url": "/libraries/danmark-testen/static/media/montserrat-bold.5704e95d.woff"
+  },
+  {
+    "revision": "9b012e21d6e311b47570b4feb56bf376",
+    "url": "/libraries/danmark-testen/index.html"
+  }
+];
