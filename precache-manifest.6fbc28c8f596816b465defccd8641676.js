d3be04369104ed3c68fd3f71ad8f4ffce2ba0829
Chris OR <chris@adaptagency.com>
2022-05-09 14:02:30 +0200

    Updated

---
+++ precache-manifest.6fbc28c8f596816b465defccd8641676.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214cac02f4d56cd66ab843f264fc7804",
+    "url": "/libraries/danmark-testen/index.html"
+  },
+  {
+    "revision": "02b039d9c5850d94ac5e",
+    "url": "/libraries/danmark-testen/static/css/main.a76efc33.chunk.css"
+  },
+  {
+    "revision": "72f93ce3f79eb9fbb8c7",
+    "url": "/libraries/danmark-testen/static/js/2.6948f763.chunk.js"
+  },
+  {
+    "revision": "c2e7ee7e61bc00b7f94a9b8f4a737ac7",
+    "url": "/libraries/danmark-testen/static/js/2.6948f763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b039d9c5850d94ac5e",
+    "url": "/libraries/danmark-testen/static/js/main.c86dbab6.chunk.js"
+  },
+  {
+    "revision": "4522c057fc749d39e609",
+    "url": "/libraries/danmark-testen/static/js/runtime-main.2b2e4f03.js"
+  },
+  {
+    "revision": "db3ed03b0ee72ae05637c0204c233a34",
+    "url": "/libraries/danmark-testen/static/media/Kvinde-med-baerbar-i-skodet.db3ed03b.jpg"
+  },
+  {
+    "revision": "b68349ec9b9a41ad2dc18e23f596bb04",
+    "url": "/libraries/danmark-testen/static/media/arrow-right.b68349ec.svg"
+  },
+  {
+    "revision": "745ab4ede44e4c88aa8dda40e1115ad3",
+    "url": "/libraries/danmark-testen/static/media/icons__dot--grey.745ab4ed.svg"
+  },
+  {
+    "revision": "bff5b07ccbc3c7422b2b060c7a2bb058",
+    "url": "/libraries/danmark-testen/static/media/icons__dot--red.bff5b07c.svg"
+  },
+  {
+    "revision": "a555067a204023a14ae6f65741a16fd7",
+    "url": "/libraries/danmark-testen/static/media/icons__heart--grey.a555067a.svg"
+  },
+  {
+    "revision": "8837f62ead22edef140eb67b6e52d994",
+    "url": "/libraries/danmark-testen/static/media/icons__heart--red.8837f62e.svg"
+  },
+  {
+    "revision": "4aec0152c75757d9c349f0a1df203cbf",
+    "url": "/libraries/danmark-testen/static/media/montserrat-bold.4aec0152.eot"
+  },
+  {
+    "revision": "5704e95d4ffd15d3c9466515fb5612c8",
+    "url": "/libraries/danmark-testen/static/media/montserrat-bold.5704e95d.woff"
+  },
+  {
+    "revision": "439848559b8c1b40ee7a0b68ac756808",
+    "url": "/libraries/danmark-testen/static/media/montserrat-regular.43984855.woff"
+  },
+  {
+    "revision": "48180df301311206eedc407d34615fe6",
+    "url": "/libraries/danmark-testen/static/media/montserrat-regular.48180df3.eot"
+  },
+  {
+    "revision": "fb9f212b6362c158fd1d82514cb64281",
+    "url": "/libraries/danmark-testen/static/media/montserrat-regular.fb9f212b.woff2"
+  }
+]);
