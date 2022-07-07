66a2ac0b613410b431ef6ca9f5cf5b4b0b9730fb
Chris OR <chris@adaptagency.com>
2022-07-07 10:40:14 +0200

    Updated

---
+++ precache-manifest.c680f9097d0634a1beb2426f7b9babc7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffbdd5e28caec9aa7f4da7a33bbf600",
+    "url": "/vendor/danmark-testen/index.html"
+  },
+  {
+    "revision": "d7bf36c4d62f54930dde",
+    "url": "/vendor/danmark-testen/static/css/main.966b6614.chunk.css"
+  },
+  {
+    "revision": "174986f807685ba84e91",
+    "url": "/vendor/danmark-testen/static/js/2.190452c7.chunk.js"
+  },
+  {
+    "revision": "c2e7ee7e61bc00b7f94a9b8f4a737ac7",
+    "url": "/vendor/danmark-testen/static/js/2.190452c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7bf36c4d62f54930dde",
+    "url": "/vendor/danmark-testen/static/js/main.88c565cf.chunk.js"
+  },
+  {
+    "revision": "d33bb37b3dee2c35c20f",
+    "url": "/vendor/danmark-testen/static/js/runtime-main.e3af63c3.js"
+  },
+  {
+    "revision": "db3ed03b0ee72ae05637c0204c233a34",
+    "url": "/vendor/danmark-testen/static/media/Kvinde-med-baerbar-i-skodet.db3ed03b.jpg"
+  },
+  {
+    "revision": "b68349ec9b9a41ad2dc18e23f596bb04",
+    "url": "/vendor/danmark-testen/static/media/arrow-right.b68349ec.svg"
+  },
+  {
+    "revision": "745ab4ede44e4c88aa8dda40e1115ad3",
+    "url": "/vendor/danmark-testen/static/media/icons__dot--grey.745ab4ed.svg"
+  },
+  {
+    "revision": "bff5b07ccbc3c7422b2b060c7a2bb058",
+    "url": "/vendor/danmark-testen/static/media/icons__dot--red.bff5b07c.svg"
+  },
+  {
+    "revision": "a555067a204023a14ae6f65741a16fd7",
+    "url": "/vendor/danmark-testen/static/media/icons__heart--grey.a555067a.svg"
+  },
+  {
+    "revision": "8837f62ead22edef140eb67b6e52d994",
+    "url": "/vendor/danmark-testen/static/media/icons__heart--red.8837f62e.svg"
+  }
+]);
